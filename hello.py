9e391c8d71e27c29613dff5f8345d6b3e213713d
Juan Arias <juan.arias@marist.edu>
2018-01-27 14:01:12 -0500

    Feedback

--- hello.py
+++ hello.py
@@ -1,21 +1,8 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> # Introduction to Programming
->>> # Author: Mary M
->>> # Date: 1/26/18
->>> def main():
-	print("Hello, instructor!")
-
-	
->>> main()
-Hello, instructor!
->>> 
+# Introduction to Programming
+# Author: Mary M
+# Date: 1/26/18
 def main():
 	print("Hello, instructor!")
 	print ("Good-bye!")
-
 	
->>> main()
-Hello, instructor!
-Good-bye!
->>> 
+main()

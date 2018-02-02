89c609787860095f89b83782c2e56df1f28d274e
m-mcgrail <35709454+m-mcgrail@users.noreply.github.com>
2018-02-02 17:58:10 -0500

    Create evens.py

---
+++ evens.py
@@ -0,0 +1,36 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #evens.py
+>>> # A code for simple loops; cmpt120 Lab 2 part 2
+>>> for i in range(10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> def main():
+	for i in range (1, 11):
+		print(2 * i)
+
+		
+>>> 
+>>> main()
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+>>> 

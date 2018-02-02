561a2a7a4eccf18ad600ee451cbc160e2b2c864c
m-mcgrail <35709454+m-mcgrail@users.noreply.github.com>
2018-02-02 17:54:24 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,27 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #madlib.py
+>>> # Code for variables and strings; lab 2 part 3
+>>> def main():
+		Noun = (input("Enter a noun:"))
+		Verb = (input("Enter an action verb:"))
+		Prep = (input("Enter a preposition:"))
+		Location = (input("Enter a location:"))
+		print("The", Noun, "likes to", Verb, Prep, "the", Location, ".")
+
+		
+>>> def main():
+		Noun = (input("Enter a noun:"))
+		Verb = (input("Enter an action verb:"))
+		Prep = (input("Enter a preposition:"))
+		Location = (input("Enter a location:"))
+		print("The", Noun, "likes to", Verb, Prep, "the", Location, ".")
+
+		
+>>> main()
+Enter a noun:cat
+Enter an action verb:jumps
+Enter a preposition:into
+Enter a location:bag
+The cat likes to jumps into the bag .
+>>> 

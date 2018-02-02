5e320e661398d54debfc798bcd319bcc6a064346
Mary <mary.mcgrail1@marist.edu>
2018-02-02 17:24:08 -0500

    Solution to Curiosity rover exercise.

---
+++ rover1.py
@@ -0,0 +1,7 @@
+def main():
+    distance = 34000000
+    speed = 186000
+    time = distance/speed
+    print("The Mars Curiosity rover can get photos of Mars to Earth in", time, "seconds when the distance is 34,000,000 miles.")
+
+main()

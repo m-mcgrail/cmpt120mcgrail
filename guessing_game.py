acb841e1dbe62a4e41ef4da994bef7851501eb90
mmcgrail <mary.mcgrail1@marist.edu>
2018-03-02 16:19:12 -0500

    Revised final version of the initial simple guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,17 @@
 #guessing_game
-#Lab 5 part 1
-def answer(guess):
-    print("Nope! Guess again")
-    guess = input("Your guess:")
-    return guess    
+#Lab 5 part 1   
 def game():
-    print("Hello, I am thinkiing of an animal. Can you guess what animal I am thinking of?:")
+    print("Hello, I am thinking of an animal. Can you guess what animal I am thinking of?:")
     animal = ('pig')
     guess = input("Your guess:")
-    while guess != animal:
-        return(answer(guess))
-    else:
-        print("You're right! I was thinking of a", animal)
-    print("Thanks for playing!")     
+    if guess !=animal:
+        False
+    while (True):
+        print("Nope! Guess again")
+        guess = input("Your guess:")
+
+        if guess ==animal:
+            print("You're right! I was thinking of a", animal)
+            return guess
+
 game()

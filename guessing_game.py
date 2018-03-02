3f22fef93d59f0573414bf96c006960c1a25f20d
mmcgrail <mary.mcgrail1@marist.edu>
2018-03-02 16:37:43 -0500

    Usability improvements to the guessing game, resolves #3 and #4.

--- guessing_game.py
+++ guessing_game.py
@@ -3,14 +3,20 @@
 def game():
     print("Hello, I am thinking of an animal. Can you guess what animal I am thinking of?:")
     animal = ('pig')
-    guess = input("Your guess:")
-    if guess !=animal:
+#lab 5 pt 2: quit function
+    stop = ('quit')
+    guess = input("Your guess:") 
+#lab 5 pt2 : lower/upper cases
+    if guess.lower() !=animal:
         False
     while (True):
-        print("Nope! Guess again")
+        print("Nope! Guess again or quit")
         guess = input("Your guess:")
-
-        if guess ==animal:
+        #quit function
+        if guess.lower() ==stop:
+            print("Goodbye, thanks for playing!")
+            return guess
+        if guess.lower() ==animal:
             print("You're right! I was thinking of a", animal)
             return guess
 

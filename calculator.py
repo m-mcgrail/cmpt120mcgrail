8a839eec7a8ccaa445defe3dd10d85954b29ff8f
mmcgrail <mary.mcgrail1@marist.edu>
2018-03-07 08:04:52 -0500

    Calculator project part 2 first revisions; added more buttons and other operators

--- calculator.py
+++ calculator.py
@@ -1,19 +1,22 @@
+#Calculator project
 from graphics import *
 #test for calculator project
-win = GraphWin('Calc', 320, 500)
+win = GraphWin('Calc', 400, 580)
 displayTextElement = Text(Point(0, 50), "")
 #creates the window
 calcGrid = [
-    [7, 8, 9, '+'],
-    [4, 5, 6, '-'],
-    [1, 2, 3, '*'],
-    ['', 0, '+/-', '/'],
-     ['', '=', '', '']
+    [7, 8, 9, '+', 'MC'],
+    [4, 5, 6, '-', 'M+'],
+    [1, 2, 3, '*', 'M-'],
+    ['.', 0, '+/-', '/', 'MR'],
+    ['^2', '%', '1/x', 'SQRT', 'MS'],
+    ['', '', '=', '', '']
+    
 ]
-buttons = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+buttons = [['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '']]
 def calcButton(x, y, value):
     button = Rectangle(Point(x,y),Point(x + 80,y + 80))
-    button.setFill('cyan')
+    button.setFill('palevioletred1')
     button.draw(win)
     text = Text(Point(x + 40, y + 40), value)
     text.draw(win)
@@ -26,16 +29,16 @@ def inside(clicked, button):
     return False
 
 def clickedButton(clicked):
-    for i in range(5):
-        for j in range(4):
+    for i in range(6):
+        for j in range(5):
             if clicked.getX() > buttons[i][j].p1.getX()and clicked.getX() < buttons[i][j].p2.getX():
                 if clicked.getY() > buttons[i][j].p1.getY()and clicked.getY() < buttons[i][j].p2.getY():
                     return i, j
     return -1, -1
 
 def createCalculatorButtons():
-    for i in range(5):
-        for j in range(4):
+    for i in range(6):
+        for j in range(5):
             buttons[i][j] = calcButton(j * 80, i * 80 + 100, calcGrid[i][j])
 
 #calculator functions
@@ -43,10 +46,14 @@ def checkInt(toCheck):
     return toCheck == '0' or toCheck == '1' or toCheck == '2' or toCheck == '3' or toCheck == '4' or toCheck == '5' or toCheck == '6' or toCheck == '7' or toCheck == '8' or toCheck == '9'
 
 def checkOperator(toCheck):
-    return toCheck == '/' or toCheck == '*' or toCheck == '+' or toCheck == '-'
+    return toCheck == '/' or toCheck == '*' or toCheck == '+' or toCheck == '-' or toCheck == '%'or toCheck == 'SQRT' or toCheck == '^2'or toCheck == '1/x' 
 
 def checkSignChange(toCheck):
     return toCheck == '+/-'
+
+#adding decimals
+def checkDecimal (toCheck):
+    return toCheck == '.'
 
 def checkEnter(toCheck):
     return toCheck == '='
@@ -57,7 +64,11 @@ def inceaseNumber(num1, num2):
        
     else :
         return (num1 * 10) + num2
-       
+#operations
+#adding decimals
+def decimal(x, y):
+    return x + '.' + y
+
 def add(x, y):
    return x + y
 
@@ -72,6 +83,15 @@ def divide(x, y):
 
 def sign(x):
     return x * -1
+#adding other operators
+def percent(x, y):
+    return y -((x / 100) * y)
+def squareRoot(x):
+    return x ** (0.5)
+def square(x):
+    return x **2
+def reciprocal(x):
+    return 1 / x
 
 def computation(num1, num2, operation):
     if(operation == '+'):
@@ -84,6 +104,15 @@ def computation(num1, num2, operation):
         return divide(num1, num2)
     if(operation == '+/-'):
         return sign(num1)
+#new operators
+    if(operation == '%'):
+        return percent(num1, num2)
+    if(operation == 'SQRT'):
+        return squareRoot(num1)
+    if(operation == '^2'):
+        return square(num1)
+    if(operation == '1/x'):
+        return reciprocal(num1)
 #Creates Calculator
 def main():
     createCalculatorButtons()
@@ -100,7 +129,7 @@ def main():
         clicked = win.getMouse()
         row, col = clickedButton(clicked)
         if row >=0:
-            buttons[row][col].setFill('pink')
+            buttons[row][col].setFill('pink1')
             
             if (checkInt(str(calcGrid[row][col]))):
                 if(len(operation) == 0):
@@ -110,7 +139,11 @@ def main():
 
             if (checkOperator(str(calcGrid[row][col]))):
                 operation = calcGrid[row][col]
-
+            #adding decimals
+            if (checkDecimal(str(calcGrid[row][col]))):
+                decimal = calcGrid[row][col]
+                displayString = decimal(firstNum, secondNum)
+            
             if(checkSignChange(str(calcGrid[row][col]))):
                 displayString = str(computation(firstNum, secondNum, '+/-'))
                 firstNum = computation(firstNum, secondNum, operation)
@@ -131,10 +164,10 @@ def main():
             displayTextElement.undraw()
             displayTextElement = Text(Point(300 - (len(displayString) * 7), 50), displayString)
             displayTextElement.draw(win)
-        for i in range(5):
-            for j in range(4):
+        for i in range(6):
+            for j in range(5):
                 if not(i == row and j == col):
-                    buttons[i][j].setFill('cyan')
+                    buttons[i][j].setFill('palevioletred1')
 
         
 main()

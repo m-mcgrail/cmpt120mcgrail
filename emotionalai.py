85f9d9d90099bbef986ad95902f0d76dd331ec0d
mmcgrail <mary.mcgrail1@marist.edu>
2018-04-13 13:10:55 -0400

    Lab 8; planned out code

---
+++ emotionalai.py
@@ -0,0 +1,30 @@
+#lab 8
+#This program is an AI that simulates human emotions and responses
+#emotionalai.py
+#variable for current emotion
+    #must be reset after new emotions
+#variables for different emotions and actions
+responses = [["happy", "playful", "sad", "afraid", "playful"],
+             ["sad", "surprised", "sad", "sad", "angry"],
+             ["angry", "surprised", "angry", "angry", "disgusted"],
+             ["disgusted", "angry", "sad", "angry", "angry"],
+             ["afraid", "happy", "sad", "sad", "surprised"],
+             ["surprised", "happy", "angry", "sad", "happy"],
+             ["playful", "happy", "sad", "angry", "playful"]]
+def primLoop():
+    #get next interaction
+        #prompt for input
+        #return desired action
+    #lookup new appropriate reaction
+        #take in users input
+        #return new emotional state
+    #Show emotion
+        #prints current emotion
+        #return emotion in current state
+
+
+def main():
+    #intro
+    #primary loop
+    #outro
+main()    

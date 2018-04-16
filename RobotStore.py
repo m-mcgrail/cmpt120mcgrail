6061a7a2ec5d75b5bc306beb8b4bfbd639480b6b
m-mcgrail <35709454+m-mcgrail@users.noreply.github.com>
2018-04-16 17:50:47 -0400

    Lap 9; progress made in lab period

--- RobotStore.py
+++ RobotStore.py
@@ -1,5 +1,40 @@
 #lab 9
 #RobotStore.py
+
+
+#look up how to better make a class
+#lab 9 part 1.a
+class ProductInfo:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+    def getProduct(self):
+        return self.name
+    def getPrice(self):
+        return self.price
+    def getQuantity(self):
+        return self.stock
+    #lab 9 part 1.b
+    def stockQuantity(self, name, stock):
+        for i in range(0,len(name)):
+            if stock[i] > 0:
+                print("There are", int(stock), name, "left in stock")
+    #lab 9 part 1.c
+    def costTotal(self, name, price):
+        orderQuan = 0 + #how much of the product are they buying
+        itemTotal = orderquan * self.price
+        costTotal = itemTotal + itemTotal
+    #lab 9 part 1.d
+    def depleteStock(self, name, stock):
+        orderQuan = #as seen above
+        stock = stock - orderQuan
+        for i in range(0,len(name)):
+            if stock[i] > 0:
+                print("There are", int(stock), name, "left in stock")
+                
+#OG code below
+
 productNames = [ "Ultrasonic range finder"
  , "Servo motor"
  , "Servo controller"
@@ -10,6 +45,7 @@ productNames = [ "Ultrasonic range finder"
 productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
 productQuantities = [ 4, 10, 5, 7, 2, 8 ]
 
+                
 def printStock():
     print()
     print("Available Products")

dd55e2299a2fe6e865520d5bd08bba97ed24624e
mmcgrail <mary.mcgrail1@marist.edu>
2018-04-23 17:16:51 -0400

    Program from lecture to help with lab 10

---
+++ simulation.py
@@ -0,0 +1,24 @@
+# simulation.py
+from simstats import SimStats
+from rballgame import RBallGame
+
+def printIntro():
+    print("This program simulates a racquetball series of games")
+
+def getInputs():
+    probA = float(input("Enter the probability of A: "))
+    probB = float(input("Enter the probability of B: "))
+    n = int(input("Number of games to simulate: "))
+    return probA, probB, n
+    
+def main():
+    printIntro()
+    probA, probB, n = getInputs()
+    stats = SimStats()
+    for i in range(n):
+        theGame = RBallGame(probA, probB)
+        theGame.play()
+        stats.update(theGame)
+    stats.printReport()
+
+main()

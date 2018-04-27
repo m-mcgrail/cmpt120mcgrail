4212cd0c76dbb45fbf7314e8e3ed197ecd7ebb20
m-mcgrail <35709454+m-mcgrail@users.noreply.github.com>
2018-04-27 15:22:23 -0400

    Add files via upload

---
+++ Calculator project/calculator.py
@@ -0,0 +1,230 @@
+#Calculator project
+from graphics import *
+#test for calculator project
+win = GraphWin('Calc', 400, 580)
+displayTextElement = Text(Point(0, 50), "")
+#creates the window
+calcGrid = [
+    [7, 8, 9, '+', 'MC'],
+    [4, 5, 6, '-', 'M+'],
+    [1, 2, 3, '*', 'M-'],
+    ['.', 0, '+/-', '/', 'MR'],
+    ['^2', '%', '1/x', 'SQRT', 'MS'],
+    ['', '', '=', '', 'C']
+    
+]
+buttons = [['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '']]
+def calcButton(x, y, value):
+    button = Rectangle(Point(x,y),Point(x + 80,y + 80))
+    button.setFill('palevioletred1')
+    button.draw(win)
+    text = Text(Point(x + 40, y + 40), value)
+    text.draw(win)
+    return button
+
+def inside(clicked, button):
+    if clicked.getX() > button.p1.getX()and clicked.getX() < button.p2.getX():
+            if clicked.getY() > button.p1.getY()and clicked.getY() < button.p2.getY():
+                return True
+    return False
+
+def clickedButton(clicked):
+    for i in range(6):
+        for j in range(5):
+            if clicked.getX() > buttons[i][j].p1.getX()and clicked.getX() < buttons[i][j].p2.getX():
+                if clicked.getY() > buttons[i][j].p1.getY()and clicked.getY() < buttons[i][j].p2.getY():
+                    return i, j
+    return -1, -1
+
+def createCalculatorButtons():
+    for i in range(6):
+        for j in range(5):
+            buttons[i][j] = calcButton(j * 80, i * 80 + 100, calcGrid[i][j])
+
+#calculator functions
+def checkInt(toCheck):
+    return toCheck == '0' or toCheck == '1' or toCheck == '2' or toCheck == '3' or toCheck == '4' or toCheck == '5' or toCheck == '6' or toCheck == '7' or toCheck == '8' or toCheck == '9'
+
+def checkOperator(toCheck):
+    return toCheck == '/' or toCheck == '*' or toCheck == '+' or toCheck == '-' or toCheck == '%'or toCheck == 'SQRT' or toCheck == '^2'or toCheck == '1/x' 
+
+def checkSignChange(toCheck):
+    return toCheck == '+/-'
+
+#adding decimals
+def checkDecimal (toCheck):
+    return toCheck == '.'
+#memory functions
+def checkMem (toCheck):
+    return toCheck == 'MC' or toCheck == 'M+' or toCheck =='M-' or toCheck == 'MR' or toCheck == 'MS'
+
+def checkEnter(toCheck):
+    return toCheck == '='
+#clear function
+def checkClear(toCheck):
+    return toCheck == 'C'
+
+def inceaseNumber(num1, num2):
+    if(num1 == 0) :
+        return num2
+       
+    else :
+        return (num1 * 10) + num2
+#operations
+#adding decimals
+def decimal(x, y):
+    return x +  (y / 10)
+
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+def sign(x):
+    return x * -1
+#adding other operators
+def percent(x, y):
+    return y -((x / 100) * y)
+def squareRoot(x):
+    return x ** (0.5)
+def square(x):
+    return x **2
+def reciprocal(x):
+    return 1 / x
+
+def computation(num1, num2, operation):
+    if(operation == '+'):
+        return add(num1, num2)
+    if(operation == '-'):
+        return subtract(num1, num2)
+    if(operation == '*'):
+        return multiply(num1, num2)
+    if(operation == '/'):
+        return divide(num1, num2)
+    if(operation == '+/-'):
+        return sign(num1)
+#new operators
+    if(operation == '%'):
+        return percent(num1, num2)
+    if(operation == 'SQRT'):
+        return squareRoot(num1)
+    if(operation == '^2'):
+        return square(num1)
+    if(operation == '1/x'):
+        return reciprocal(num1)
+    if(operation == '.'):
+        return decimal(num1, num2)
+    return num1
+#memory functions
+# There is a problem with the memory operations
+def memory(displayString, operation):
+    print(displayString)
+    if(operation =='MS'):
+        savedMem = int(displayString)
+        return savedMem
+    if(operation == 'M+'):
+       savedMem = int(displayString) + savedMem
+       return savedMem
+    if(operation == 'MC'):
+        savedMem = 0
+        return savedMem
+    if(operation == 'MR'):
+        return savedMem
+    if(operation == 'M-'):
+        savedMem = int(displayString) - savedMem
+        return savedMem
+        
+#clear function
+def clear(operation):
+    if(operation == 'C'):
+        displayString = str(0)
+        return displayString
+    
+#Creates Calculator
+def main():
+    createCalculatorButtons()
+    displayString = ''
+    displayTextElement = Text(Point(0, 50), "")
+    displayTextElement.draw(win)
+
+    nextDecimal = False
+    firstNum = 0
+    secondNum = 0
+    operation = ''
+    savedMem = 0
+    
+    while 1 == 1:
+        
+        clicked = win.getMouse()
+        row, col = clickedButton(clicked)
+        if row >=0:
+            buttons[row][col].setFill('pink1')
+            
+            if(nextDecimal != True):
+                if (checkInt(str(calcGrid[row][col]))):
+                    if(len(operation) == 0):
+                        firstNum = inceaseNumber(firstNum, calcGrid[row][col])
+                    else :
+                        secondNum = inceaseNumber(secondNum, calcGrid[row][col])
+
+                if (checkOperator(str(calcGrid[row][col]))):
+                    operation = calcGrid[row][col]
+                #adding decimals
+                if (checkDecimal(str(calcGrid[row][col]))):
+                    nextDecimal = True
+                                    
+                if(checkSignChange(str(calcGrid[row][col]))):
+                    displayString = str(computation(firstNum, secondNum, '+/-'))
+                    firstNum = computation(firstNum, secondNum, operation)
+                    secondNum = 0
+                    operation = ''
+                #clear function
+                if(checkClear(str(calcGrid[row][col]))):
+                    displayString = str(clear(calcGrid[row][col]))
+                    firstNum = 0
+
+                #memory functions
+                if(checkMem(str(calcGrid[row][col]))):
+                    operation = calcGrid[row][col]
+                    displayString = str(computation(firstNum, secondNum, operation))
+                    print(displayString)
+                    savedMem = memory(displayString, operation)    
+                    
+                elif (checkEnter(str(calcGrid[row][col]))):
+                    displayString = str(computation(firstNum, secondNum, operation))
+                    firstNum = computation(firstNum, secondNum, operation)
+                    secondNum = 0
+                    operation = ''
+                else :
+                    if(len(operation) == 0):
+                        displayString = (str(firstNum) + operation)
+                    else:
+                        displayString = (str(firstNum) + operation + str(secondNum))
+            else:
+                if(checkInt(str(calcGrid[row][col]))):
+                    if(secondNum == 0):
+                        firstNum = decimal(firstNum, calcGrid[row][col])
+                        nextDecimal = False
+                        displayString = str(firstNum)
+                    else:
+                        secondNum = decimal(secondNum, calcGrid[row][col])
+                        nextDecimal = False
+                        displayString = str(firstNum) + operation + str(secondNum)
+
+            displayTextElement.undraw()
+            displayTextElement = Text(Point(300 - (len(displayString) * 7), 50), displayString)
+            displayTextElement.draw(win)
+        for i in range(6):
+            for j in range(5):
+                if not(i == row and j == col):
+                    buttons[i][j].setFill('palevioletred1')
+
+        
+main()
---
+++ Calculator project/calculator3.py
@@ -0,0 +1,145 @@
+#Calculator Project part 3
+#calculator3.py
+from graphics import *
+from button import Button
+import math
+
+# JA: There seems to be a problem with the clear button
+
+def createCalculatorGui():
+    #changed dimensions to make room for scientific mode
+    win = GraphWin("calculator", 900, 500)
+    win.setCoords(0, .5, 9, 6.75)
+    win.setBackground("aliceblue")
+    #scientific buttons
+    bSpecs = [(1,1,'ln'), (2,1,'10^x'), (3,1,"( )"),(4,1,'0'), (5,1,'.'),
+          (1,2,'tan^-1'), (2,2,'tan'), (3,2,'1'), (4,2,'2'), (5,2,'3'), (6,2,'+'), (7,2,'-'),
+          (1,3,'cos^-1'), (2,3,'cos'), (3,3,'4'), (4,3,'5'), (5,3,'6'), (6,3,'*'), (7,3,'/'),
+          (1,4,'sin^-1'), (2,4,'sin'), (3,4,'7'), (4,4,'8'), (5,4,'9'), (6,4,'<-'),(7,4,'C'),
+          (1,5,'log'), (2,5,'x^y'), (3,5,'MC'),(4,5,'M+'),(5,5,'M-'),(6,5,'MR'),(7,5,'MS')]
+    
+    buttons = createButtons(bSpecs, win)
+    display = createDisplay(win)
+    runDisp = runningDisplay(win)
+    return buttons, display, win, runDisp
+
+def createButtons(bSpecs, win):
+    buttons = []
+    for cx, cy, label in bSpecs:
+        buttons.append(Button(win, Point(cx, cy), .75, .75, label))
+    buttons.append(Button(win, Point(6.5,1), 1.75, .75, "="))
+    for b in buttons:
+        b.activate()
+    return buttons
+
+def createDisplay(win):
+    bg = Rectangle(Point(.5, 5.75), Point(5, 6.5))
+    bg.setFill('white')
+    bg.draw(win)
+    text = Text(Point(2.75, 6.15), "")
+    text.draw(win)
+    text.setFace("courier")
+    text.setStyle("bold")
+    text.setSize(16)
+    return text
+def runningDisplay(win):
+    rd = Rectangle(Point(5.5, 5.75), Point(7.4, 6.5))
+    rd.setFill('azure2')
+    rd.draw(win)
+    text = Text(Point(6.75,6), "")
+    text.draw(win)
+    text.setFace("courier")
+    text.setStyle("bold")
+    text.setSize(16)
+    return text
+    #word = Text(Point(6.75,6), "")
+    #word.draw(win)
+    #word.setFace("courier")
+    #word.setStyle("bold")
+    #word.setSize(12)
+    #return word
+
+def getButtonPressed(buttons, calc):
+    while True:
+        p = calc.getMouse()
+        for b in buttons:
+            if b.clicked(p):
+                return b.getLabel()
+
+def processButton(key, display, runDisp, memory):
+    text = display.getText()
+    memory = 0
+    base = math.e
+    if key == "<-":
+        display.setText(text[:-1])
+    elif key == "C":
+        display.setText("")
+    #memory keys
+    elif key == "MC":
+        memory = 0
+    elif key == "M+":
+        memory == memory + text
+    elif key == "M-":
+        memory == memory - text
+    elif key == "MR":
+        display.setText(result)
+    elif key == "MS":
+        memory = display.setText(memory)
+    #science keys
+    elif key == "log":
+        display.setText(math.log10(int(text)))
+    elif key == "x^y":
+        display.setText(int(text)**int(text))
+    elif key == "sin^-1":
+        display.setText(math.asin(int(text)))
+        try:
+                result = (math.asin(int(text)))
+        except:
+                print("Unable to divide by 0")
+                result = 0
+    elif key == "sin":
+        display.setText(math.sin(int(text)))
+    elif key == "cos^-1":
+        display.setText(math.acos(int(text)))
+        try:
+                result = (math.acos(int(text)))
+        except:
+                print("Unable to divide by 0")
+                result = 0
+    elif key == "cos":
+        display.setText(math.cos(int(text)))
+    elif key == "tan^-1":
+        display.setText(math.atan(int(text)))
+        try:
+                result = (math.atan(int(text)))
+        except:
+                print("Unable to divide by 0")
+                result = 0
+    elif key == "tan":
+        display.setText(math.tan(int(text)))
+    elif key == "ln":
+        display.setText(math.log(int(text)))
+    elif key == "10^x":
+        display.setText(10 ** int(text))
+    elif key == "( )":
+        text = [(text)]
+    elif key == "=":
+        try:
+            result = eval(text)
+        except:
+            result = "ERROR"
+        display.setText(result)
+    else:
+        display.setText(text + key)
+    runDisp.setText(text + key)
+def main():
+    buttons, display, calc, runDisp = createCalculatorGui()
+    memory = 0
+    while True:
+        key = getButtonPressed(buttons, calc)
+        print(key)
+        processButton(key, display, runDisp, memory)
+
+
+main()
+
